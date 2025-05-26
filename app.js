69949cd8dc14e1dcbab2e86ed29578efb3a7afa2
GisenTheCat <GisenSpelar@gmail.com>
2025-05-26 12:24:56 +0200

    work in progress deltatime

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const pathname = document.location.pathname;
 const slide = Number(pathname.substring(pathname.length - 6, pathname.length - 5));
+let lastFrame = new Date();
 
 document.addEventListener('DOMContentLoaded', () => {
     document.body.addEventListener('keydown', (event) => {
@@ -14,23 +15,25 @@ document.addEventListener('DOMContentLoaded', () => {
         case 1:
             const img = document.getElementById('screensaver');
             let x = 100, y = 100;
-            let dx = 4, dy = 4;
+            let dx = 0.4, dy = 0.4;
         
             function move() {
-              const bounds = img.getBoundingClientRect();
-              const w = window.innerWidth;
-              const h = window.innerHeight;
-        
-              if (x + bounds.width >= w || x <= 0) dx = -dx;
-              if (y + bounds.height >= h || y <= 0) dy = -dy;
-        
-              x += dx;
-              y += dy;
-        
-              img.style.left = x + 'px';
-              img.style.top = y + 'px';
-        
-              requestAnimationFrame(move);
+                let dt = new Date() - lastFrame;
+                lastFrame = new Date();
+                const bounds = img.getBoundingClientRect();
+                const w = window.innerWidth;
+                const h = window.innerHeight;
+                
+                if (x + bounds.width == w || x == 0) dx = -dx;
+                if (y + bounds.height == h || y == 0) dy = -dy;
+                
+                x += dx * dt;
+                y += dy * dt;
+                
+                img.style.left = x + 'px';
+                img.style.top = y + 'px';
+                
+                requestAnimationFrame(move);
             }
         
             move();
